f13b07eec89db8f7bb0f0e48113b67503154c4e1
remcohofmans <remco.hofmans@student.kuleuven.be>
2024-11-15 13:50:04 +0100

    Style password recovery page and provide navigation link to page

--- src/Components/LoginRegister.jsx
+++ src/Components/LoginRegister.jsx
@@ -218,8 +218,8 @@ const LoginRegister = ({ loginWithEmail, signUpWithEmail, error }) => {
 
                 {passwordFeedback && <p className="text-red-600 text-sm">{passwordFeedback}</p>}
 
-                             {/* Forgot Password Link */}
-                             <div className="text-right mt-2">
+                {/* Forgot Password Link */}
+                <div className="text-right mt-2">
                   <a
                     href="/forgotPassword"
                     className="text-[#e11d48] hover:text-[#be123c] text-sm"
--- src/Components/PasswordRecovery.jsx
+++ src/Components/PasswordRecovery.jsx
@@ -39,18 +39,18 @@ const PasswordRecovery = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-[#fbf6f0]">
       <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full">
-        <h2 className="text-2xl font-bold text-gray-800 mb-4">Herstel Paswoord</h2>
-        
+        <h2 className="text-2xl font-bold text-gray-800 mb-4">Herstel paswoord</h2>
+
         {message && <div className="bg-green-100 text-green-800 p-4 rounded mb-4">{message}</div>}
         {error && <div className="bg-red-100 text-red-800 p-4 rounded mb-4">{error}</div>}
-        
+
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-700">E-mailadres</label>
+            <label htmlFor="email" className="block text-gray-700 mb-2">E-mailadres</label>
             <input
               type="email"
               id="email"
-              className="w-full px-4 py-2 border rounded-lg text-gray-800"
+              className="w-full py-3 px-12 bg-gray-50 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-[#fda4af]"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               placeholder="E.g. john.doe@example.com"
@@ -60,7 +60,7 @@ const PasswordRecovery = () => {
 
           <button
             type="submit"
-            className={`w-full py-2 text-white rounded-lg ${isLoading ? 'bg-gray-400' : 'bg-[#f43f5e] hover:bg-[#e11d48]'}`}
+            className={`w-full py-2 text-white rounded-lg ${isLoading ? 'bg-gray-400' : 'bg-[#f43f5e] hover:bg-[#e11d48]'} focus:ring-2 focus:ring-offset-2 focus:ring-[#f43f5e]`}
             disabled={isLoading}
           >
             {isLoading ? 'Zenden...' : 'Verzend Link'}
