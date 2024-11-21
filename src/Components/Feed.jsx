fd41161a82242198d4c9d7d5a349df5089bdb474
Samih Ouhmad <samih.ouhmad@student.kuleuven.be>
2024-11-21 20:12:49 +0100

    small cleanup of base64, we do not need

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -59,9 +59,8 @@ const Feed = () => {
 
       const { data: fetchedUsers } = await supabase
         .from('users')
-        .select('id, birthday, name, profilepictureBASE64, city, facility, gender')
+        .select('id, birthday, name, city, facility, gender')
         .not('name', 'is', null)
-        .not('profilepictureBASE64', 'is', null)
         .not('birthday', 'is', null)
         .not('city', 'is', null)
         .limit(USERS_TO_FETCH);
@@ -103,7 +102,6 @@ const Feed = () => {
             facility: potentialUser.facility,
             birthday: potentialUser.birthday,
             age,
-            profilePicture: potentialUser.profilepictureBASE64,
             hobbies: preferencesData?.hobbies ? JSON.parse(preferencesData.hobbies) : [],
             distance,
           };
@@ -129,8 +127,7 @@ const Feed = () => {
     style: {
       backgroundColor: index % 2 === 0 ? '#fff1f2' : '#881337',
       textColor: index % 2 === 0 ? '#881337' : '#fff1f2',
-    },
-    imageURI: `data:image/jpeg;base64,${user.profilePicture}`,
+    }
   }));
 
   const handleSpinClick = () => {
