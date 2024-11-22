178d097dbbc518a24cfb86f1c19072fff5fcae2b
Matthieu Loix <matthieu.loix@student.kuleuven.be>
2024-11-22 11:28:42 +0100

    Connected supabase

--- src/Components/SubscriptionPlans.jsx
+++ src/Components/SubscriptionPlans.jsx
@@ -2,8 +2,9 @@ import TopNavigationBar from './TopNavigationBar';
 import { Check } from 'lucide-react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
-import React, { useState, useCallback } from 'react';
+import React, { useState, useEffect} from 'react';
 import { ToggleSlider }  from "react-toggle-slider";
+import { supabase } from '../lib/helper/supabaseClient'; 
 
 const Subscription = () => {
     const subscriptionBenefitItem = {icon: faCircleCheck, label: Check};
@@ -12,12 +13,24 @@ const Subscription = () => {
     const [selectedSubscription, setSelectedSubscription] = useState(false);
     const [inputVal, setInputVal] = useState('');
     const [payAnnually, setPayAnnually] = useState(false);
+    const [userId, setUserId] = useState(null);
+
 
     const monthlyPrices = {basis: 10, gevorderd: 17, elite: 20}
     const annualPrices = {basis: 110, gevorderd: 187, elite: 220}
     const annualPricesSaved = {basis: 120, gevorderd: 204, elite: 240}
 
-    
+    useEffect(() => {
+        const fetchUserData = async () => {
+          const { data: { session } } = await supabase.auth.getSession();
+          if (session) {
+            setUserId(session.user.id);
+            console.log("Session user ID:", session.user.id);
+          }
+        };
+        fetchUserData();
+      }, []);
+
     const openSubscriptionModal = (subscription) => {
         setSelectedSubscription(subscription);
         setIsSubscriptionModalOpen(true);
@@ -25,7 +38,7 @@ const Subscription = () => {
 
     const closeSubscriptionModal = () => {
         setSelectedSubscription(false);
-        setIsSubscriptionModalOpen(null);
+        setIsSubscriptionModalOpen(false);
         setInputVal('');
     };
 
@@ -37,10 +50,57 @@ const Subscription = () => {
         setIsFeedbackModalOpen(false);
     };
 
-    const handleSubscribe = () => {
+    const handleSubscribe = async (e) => {
+        console.log("subscriptionhandler entered");
+        e.preventDefault();
+  
+        if (inputVal !== 'BEVESTIG') return;
         //send mail to director using chargebee :)
-        closeSubscriptionModal();
+        const { data: { session } } = await supabase.auth.getSession();
+
+        const subscriptonFields = {
+                    user_id: userId,
+                    subscription: selectedSubscription,
+                    //end_date: payAnnually ? supabase.sqlsupabase.sql`CURRENT_DATE + INTERVAL '1 year'` : supabase.sql`CURRENT_DATE + INTERVAL '1 month'`,
+                    annual_payment: payAnnually,
+                    active:true
+                }
+        const {data: existingSubscription, error: fetchFailed} = await supabase
+            .from('subscriptions')
+            .select('*')
+            .eq('user_id', userId)
+            .single();
+
+        if (fetchFailed) {
+            console.log("no existing subscription", fetchFailed) 
+            //return;
+        }
+        
+        if (existingSubscription) {
+            const { error } = await supabase
+                .from('subscriptions')
+                .update(subscriptonFields)
+                .eq('user_id', userId);
+            if (error) {
+                console.log("update failed", error);
+                return;
+            }
+            console.log("subscription updated");
+
+        }
+        else {
+            const { error } = await supabase
+                .from('subscriptions')
+                .insert([subscriptonFields])
+            if (error){
+                console.log("inserting subscription failed", error);
+            }
+            console.log("subscription inserted");
+        }
+        
         openFeedbackModal();
+        closeSubscriptionModal();
+
     };
 
     return (
@@ -56,6 +116,7 @@ const Subscription = () => {
                         className="flex flex-col items-center justify-center m-16 max-w-sm p-6 rounded-lg drop-shadow-lg sm:p-8"
                         style={{ backgroundColor: '#be123c' }}
                     >
+                        <form onSubmit={handleSubscribe}>
                         
                         <h5 className="mb-4 text-2xl font-bold text-center" style={{ color: '#ffe4e6' }}>
                             Bevestig je aankoop!
@@ -63,27 +124,27 @@ const Subscription = () => {
                         
 
                         <div className="flex flex-col items-center text-center space-y-4" style={{ color: '#ffe4e6' }}>
-                        <div className="flex items-baseline space-x-1">
-                            <span className="text-3xl font-semibold">€</span>
-                            <span className="text-3xl font-extrabold tracking-tight">{payAnnually ? annualPrices[selectedSubscription] : monthlyPrices[selectedSubscription]}</span>
-                            <span className="text-xl font-normal" style={{ color: '#fecdd3' }}>/maand</span>
-                        </div>
+                            <div className="flex items-baseline space-x-1">
+                                <span className="text-3xl font-semibold">€</span>
+                                <span className="text-3xl font-extrabold tracking-tight">{payAnnually ? annualPrices[selectedSubscription] : monthlyPrices[selectedSubscription]}</span>
+                                <span className="text-xl font-normal" style={{ color: '#fecdd3' }}>/maand</span>
+                            </div>
 
-                        <p className="text-sm">
-                            Ben je zeker dat je wilt veranderen naar het <span className="font-bold">{selectedSubscription}</span>{' '}
-                            abonnement? Zo ja, type: <span className="font-bold">BEVESTIG</span>
-                        </p>
+                            <p className="text-sm">
+                                Ben je zeker dat je wilt veranderen naar het <span className="font-bold">{selectedSubscription}</span>{' '}
+                                abonnement? Zo ja, type: <span className="font-bold">BEVESTIG</span>
+                            </p>
 
-                        <input
-                            type="text"
-                            placeholder="BEVESTIG"
-                            value={inputVal}
-                            onChange={e => setInputVal(e.target.value)}
-                            className="w-full p-2 border-2 rounded-lg border-rose-300 text-rose-700 focus:ring-rose-500 focus:border-rose-500 outline-none"
-                            style={{ backgroundColor: '#ffe4e6' }}
-                        />
+                            <input
+                                type="text"
+                                placeholder="BEVESTIG"
+                                value={inputVal}
+                                onChange={e => setInputVal(e.target.value)}
+                                className="w-full p-2 border-2 rounded-lg border-rose-300 text-rose-700 focus:ring-rose-500 focus:border-rose-500 outline-none"
+                                style={{ backgroundColor: '#ffe4e6' }}
+                            />
                         </div>
-                        <form onSubmit={handleSubscribe} className='flex items-start justify-between w-full space-x-4'>
+                        <div  className='flex items-start justify-between w-full space-x-4'>
                             <button 
                                 onClick={closeSubscriptionModal}
                                 type="button"
@@ -94,17 +155,19 @@ const Subscription = () => {
                             >
                                 Terug
                             </button>
-
-                            <button
-                                type="submit"
-                                disabled={inputVal!=='BEVESTIG'}
-                                className="mt-6 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center transition duration-300"
-                                style={{ color: '#881337', backgroundColor: '#fff1f2' }}
-                                onMouseOver={(e) => (e.target.style.backgroundColor = '#fecdd3')}
-                                onMouseOut={(e) => (e.target.style.backgroundColor = '#fff1f2')}
-                            >
-                                Abonneer
-                            </button>
+                            
+                                <button
+                                    type="submit"
+                                    disabled={inputVal!=='BEVESTIG'}
+                                    className="mt-6 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center transition duration-300"
+                                    style={{ color: '#881337', backgroundColor: '#fff1f2' }}
+                                    onMouseOver={(e) => (e.target.style.backgroundColor = '#fecdd3')}
+                                    onMouseOut={(e) => (e.target.style.backgroundColor = '#fff1f2')}
+                                >
+                                    Abonneer
+                                </button>
+                            
+                        </div>
                         </form>
                     </div>
                 </div>
