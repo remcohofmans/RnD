19acb86ef218bb8d8be1f0770760b84a93243b11
remcohofmans <remco.hofmans@student.kuleuven.be>
2024-11-22 22:43:54 +0100

    Removed white spacing in small screen mode

--- src/Components/Auth/LoginRegister.jsx
+++ src/Components/Auth/LoginRegister.jsx
@@ -233,7 +233,7 @@ const LoginRegister = () => {
   const imageSrc = happyPeople;
 
   return (
-    <div className={`w-full ${isMobile ? 'h-auto object-contain' : 'h-auto object-contain'}`}>
+    <div className={"w-full h-full"}>
 
       {/* Split Layout Container */}
       <div className="flex flex-col md:flex-row items-cover">
@@ -288,7 +288,7 @@ const LoginRegister = () => {
         </div>
 
         {/* Right Half */}
-        <div className="w-full flex md:flex-col justify-center p-12 bg-rose-50" >
+        <div className="w-full h-screen flex md:flex-col items-center justify-center p-12 bg-rose-50" >
           <div className="w-full max-w-md mx-auto">
             <h2 className="text-3xl font-bold text-[#be123c] text-center mb-8">{isLogin ? 'Welkom!' : 'Registreer'}</h2>
 
