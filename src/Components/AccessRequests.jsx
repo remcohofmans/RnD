1a4c15e563c19a3273ea58c1525288853e8ca016
Remco Hofmans <remco.hofmans@student.kuleuven.be>
2024-11-26 19:40:48 +0100

    Some unused imports removed

--- src/Components/AccessRequests.jsx
+++ src/Components/AccessRequests.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useAuth } from '../hooks/AuthContext';
-import { useNavigate } from 'react-router-dom';
+
 
 const AccessRequests = () => {
   const { user, fetchUsersForMentor, fetchProfilePictureUrl, updateAccessStatus } = useAuth();
