b830dfa54c2a02fd84770998ec1ef362f8d3ceac
Remco Hofmans <remco.hofmans@student.kuleuven.be>
2024-11-27 13:51:20 +0100

    Reset service role tot public anon key to pass RLS

--- src/lib/helper/supabaseClient.js
+++ src/lib/helper/supabaseClient.js
@@ -1,13 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-// Server-side client
-export const supabaseServer = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
-);
-
-// Client-side client
-export const supabaseClient = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
+// Create a single supabase client for interacting with your database
+export const supabase = createClient(
+    process.env.REACT_APP_SUPABASE_URL,
+    process.env.REACT_APP_SUPABASE_ANON_KEY
+  );
