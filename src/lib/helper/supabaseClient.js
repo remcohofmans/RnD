d43f81126ae4389b34a08b121c80a2b3d8863669
Remco Hofmans <remco.hofmans@student.kuleuven.be>
2024-11-27 14:17:44 +0100

    Test

--- src/lib/helper/supabaseClient.js
+++ src/lib/helper/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl =  process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
 // Create a single supabase client for interacting with your database
-export const supabase = createClient(
-    process.env.REACT_APP_SUPABASE_URL,
-    process.env.REACT_APP_SUPABASE_ANON_KEY
-  );
+export const supabase = createClient(supabaseUrl, supabaseKey);
