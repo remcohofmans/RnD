66a32ecb8d73a095dca0b6a7ffc7f692c78da214
Samih Ouhmad <samih.ouhmad@student.kuleuven.be>
2024-12-10 14:37:25 +0100

    Meessage for friend feed now is also green when you like

--- src/Components/Feed/UserCard.jsx
+++ src/Components/Feed/UserCard.jsx
@@ -6,7 +6,6 @@ import {
   faBuilding,
   faHeart,
   faStar,
-  faComment,
   faThumbsUp,
 } from '@fortawesome/free-solid-svg-icons';
 
@@ -168,7 +167,7 @@ const UserCard = ({ user, currentUserId, showLoveButton = true, theme = 'pink' }
 
   return (
     <div className={`relative user-card ${currentTheme.cardBg} rounded-lg shadow-lg p-6 mb-6 w-80 mx-auto`}>
-      {alertMessage && <CustomAlert message={alertMessage} />}
+      {alertMessage && <CustomAlert message={alertMessage} theme={theme} />} {/* Pass the theme to CustomAlert */}
       <CarouselCard userId={user.id} />
       <h2 className={`name text-2xl font-semibold ${currentTheme.textColor} text-center`}>{user.name}</h2>
       <div className={`info text-left mt-4 ${currentTheme.textColor}`}>
